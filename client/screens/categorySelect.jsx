6b504e929f397d1e12cbd49e151d9841658a49c9
Mark-42ko <108774056+Mark-42ko@users.noreply.github.com>
2022-10-13 09:25:55 +0900

    bug fiexd
    bug fiexd

--- client/screens/categorySelect.jsx
+++ client/screens/categorySelect.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Pressable, FlatList } from "react-native";
 import { Card, Searchbar, Title, Paragraph, Chip, Modal, Portal, Provider } from 'react-native-paper';
+import CategoryStoreCard from "../components/CategoryStoreCard";
 import { getCategoryStore } from "../util/store";
 
 function CategorySelectScreen({ route, navigation }) {
@@ -13,38 +14,21 @@ function CategorySelectScreen({ route, navigation }) {
             // console.log(received);
             setStoreDatas(received.datas);
         })
-
     }, [])
 
-    const pressHandle = () => {
-        console.log("카테고리에서 가게 상세 페이지 들어가는곳");
-        //todo navigate params 설정해야함.
-        navigation.navigate("storeInfo");
-    }
+    // const pressHandle = (one) => {
+    //     console.log("카테고리에서 가게 상세 페이지 들어가는곳");
+    //     //todo navigate params 설정해야함.
+    //     navigation.navigate("storeInfo", { datas: one, place: one.RSTR_RDNMADR, places: one.RSTR_LNNO_ADRES, ph: one.RSTR_TELNO });
+    // }
 
     return (
         <View style={styles.container}>
-            <FlatList data={storeDatas} renderItem={one => {
-                return (
-                    <Pressable
-                        style={({ pressed }) => pressed ? { opacity: 0.8 } : null}
-                        onPress={pressHandle}
-                    >
-                        <Card style={{ width: "98%", margin: 5 }}>
-                            <Card.Content>
-                                {/* <Title>Card title</Title> */}
-                                {one.item.rstrImg ?
-                                    <Card.Cover source={{ uri: one.item.rstrImg?.RSTR_IMG_URL }} />
-                                    :
-                                    <Card.Cover source={require("../assets/store_defaultImage.png")} />
-                                }
-                                <Title>{one.item.RSTR_NM}</Title>
-                                <Paragraph numberOfLines={2}>{one.item.RSTR_INTRCN_CONT}</Paragraph>
-                            </Card.Content>
-                        </Card>
-                    </Pressable>
-                )
-            }} />
+            {storeDatas &&
+                <FlatList data={storeDatas} renderItem={({ index, item }) => {
+                    return <CategoryStoreCard data={item.RSTR_ID} />
+                }} />
+            }
         </View>
     );
 }
