d3254976c16ca131eac269b711bc19a9b244c2fd
mark42 <esh8876@naver.com>
2022-10-14 17:08:45 +0900

    choi update
    choi update

--- client/components/HistoryCard.jsx
+++ client/components/HistoryCard.jsx
@@ -1,14 +1,26 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text, Pressable, FlatList, Button } from "react-native";
+import { StyleSheet, View, Text, Pressable, FlatList, Button, Image } from "react-native";
 import { Card, Searchbar, Title, Paragraph, Chip, Modal, Portal, Provider } from 'react-native-paper';
 import { format } from "date-fns";
 import ko from "date-fns/esm/locale/ko/index.js"
-import { useNavigation } from "@react-navigation/native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { getStoreImageRequest } from "../util/store";
 
 function HistoryCard({ route, data }) {
     // console.log(data);
     const navigation = useNavigation();
     const [disable, setDisable] = useState(true);
+    const isFocused = useIsFocused();
+
+    const [image, setImage] = useState();
+
+    useEffect(() => {
+        !async function () {
+            const storeImage = await getStoreImageRequest(data.RSTR_ID);
+            setImage(storeImage.datas[0]?.RSTR_IMG_URL);
+            // setImage(storeImage);
+        }()
+    }, []);
 
     useEffect(() => {
         const nowDate = format(new Date(), 'yyyy-MM-dd');
@@ -20,7 +32,7 @@ function HistoryCard({ route, data }) {
             // console.log("날짜가 같고 시간이 지났음");
             setDisable(false);
         }
-    }, [])
+    }, [isFocused]);
 
     useEffect(() => {
         // 리뷰가 등록되어 있는 이용내역이라면 리뷰 남기기 버튼 비활성화
@@ -33,7 +45,13 @@ function HistoryCard({ route, data }) {
 
     return (
         <Card style={{ margin: 5 }}>
-            <Card.Content>
+            <Card.Content style={styles.innerContainer}>
+            {image ?
+                <Image resizeMode="cover" style={styles.image} source={{ uri: image }} />
+                :
+                <Image resizeMode="cover" style={styles.image} source={require("../assets/store.png")} />
+            }
+            <View>
                 <Title>{data.getRstr?.RSTR_NM}</Title>
                 <Paragraph>
                     예약 날짜: {data.date} / 예약 시간: {data.time}
@@ -44,15 +62,14 @@ function HistoryCard({ route, data }) {
                 <Paragraph>
                     요구사항 : {data.message}
                 </Paragraph>
-
                 <View style={{ marginTop: 10 }}>
                     <Button
                         title="리뷰 남기기"
                         disabled={disable}
                         onPress={() => navigation.navigate("writeReview", { RSTR_ID: data.RSTR_ID, _ID: data._id })}
                     />
-
                 </View>
+            </View>
             </Card.Content>
         </Card>
     );
@@ -65,6 +82,15 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         margin: 5
         // justifyContent: "center",
+    },
+    innerContainer : {
+        flex : 1,
+        flexDirection : "row"
+    },  
+    image: {
+        margin: 4.5,
+        height: 125,
+        width: 125
     },
 });
 
